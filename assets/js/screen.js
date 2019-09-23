ff1961df6fee828410a6ac04a629f7d4074000b8
Luis Roy <luisroyrsg@gmail.com>
2019-09-23 18:47:56 +0800

    some fix

--- assets/js/screen.js
+++ assets/js/screen.js
@@ -21,4 +21,42 @@ $(document).ready(function(){
             
         }       
     });
+
+
+    // -----------------  JS for Manage-account-details -------------------------
+    $('.mad-account-changes .mad-nav ul li').click(function(){
+        $('.mad-account-changes .mad-nav ul li').removeClass('mad-active');
+        $(this).addClass('mad-active');
+        let target = $(this).attr('id');
+        $('.mad-account-changes .mad-nav-content').addClass('display-none');
+        $('#'+target+'-content').removeClass('display-none');
+    })
+    $('.mad-moving-money .mad-nav ul li').click(function(){
+        $('.mad-moving-money .mad-nav ul li').removeClass('mad-grey-active');
+        $(this).addClass('mad-grey-active');
+        let target = $(this).attr('id');
+        $('.mad-moving-money .mad-nav-content').addClass('display-none');
+        $('#'+target+'-content').removeClass('display-none');
+    })
+    $('#mad-transfer-wire-content .mad-tab ul li').click(function(){
+        $('#mad-transfer-wire-content .mad-tab ul li').removeClass('mad-tab-active');
+        $(this).addClass('mad-tab-active');
+        let target = $(this).attr('id');
+        $('#mad-transfer-wire-content .mad-tab-content').addClass('display-none');
+        $('#'+target+'-content').removeClass('display-none');
+    })
+    $('#mad-schwab-accounts-content .mad-tab ul li').click(function(){
+        $('#mad-schwab-accounts-content .mad-tab ul li').removeClass('mad-tab-active');
+        $(this).addClass('mad-tab-active');
+        let target = $(this).attr('id');
+        $('#mad-schwab-accounts-content .mad-tab-content').addClass('display-none');
+        $('#'+target+'-content').removeClass('display-none');
+    })
+    $('#mad-request-check-content .mad-tab ul li').click(function(){
+        $('#mad-request-check-content .mad-tab ul li').removeClass('mad-tab-active');
+        $(this).addClass('mad-tab-active');
+        let target = $(this).attr('id');
+        $('#mad-request-check-content .mad-tab-content').addClass('display-none');
+        $('#'+target+'-content').removeClass('display-none');
+    })
 });
